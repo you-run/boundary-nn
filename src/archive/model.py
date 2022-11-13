5da0582d341d5558e32f6b9cc89ed99f9eb7a9e5
hyeeiin <jgheon.kim@gmail.com>
2022-11-13 23:53:40 +0900

    <fix> minor change (indentation)

--- src/archive/model.py
+++ src/archive/model.py
@@ -2,6 +2,30 @@ import torch
 import torch.nn as nn
 
 
+class ConvBlock(nn.Module):
+    def __init__(
+        self, in_channels, out_channels,
+        kernel_size=3, stride=1, padding=1, pool_size=2
+    ):
+        super().__init__()
+        self.conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding
+        )
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+        self.act = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=pool_size, stride=pool_size)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.act(out)
+        out = self.pool(out)
+        return out
+
 class BoundaryRecognizer(nn.Module):
     def __init__(self, latent_dim=128):
         super().__init__()
