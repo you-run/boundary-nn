f2a04ecdebd5588eeddb2f3cd87373e067a2c486
yeowon010306 <yeowon010306@g.skku.edu>
2022-12-13 16:42:31 +0900

    <fix> cae preprocessing

--- preprocessing/image_for_cae_preprocessing.py
+++ preprocessing/image_for_cae_preprocessing.py
@@ -1,5 +1,6 @@
 import os
 
+import glob
 import skvideo
 import skvideo.io
 from tqdm import tqdm
@@ -13,18 +14,18 @@ os.makedirs(os.path.join(SAVE_PATH, 'HB'), exist_ok=True)
 os.makedirs(os.path.join(SAVE_PATH, 'NB'), exist_ok=True)
 os.makedirs(os.path.join(SAVE_PATH, 'SB'), exist_ok=True)
 
-with open('./video_names.txt','r') as f:
-    v_names = f.read().splitlines()
+file_path_list = glob.glob(VIDEO_PATH + "/**/*.mp4", recursive=True)
+for n, file_path in enumerate(file_path_list):
+    # file_path: ../data/MemSeg_clips/HB/HB_1.mp4
+    boundary_type, video_name_with_ext = file_path.split('/')[-2:] # HB, HB_1.mp4 
+    video_name = video_name_with_ext.split('.')[0] # HB_1
+    print(f"[{n + 1}/{len(file_path_list)}] Now Processing {video_name} ...")
 
-    for data in tqdm(v_names): # data: HB_1.mp4
-        data_name = data.split(".")[0] # data_name: HB_1
-        data_type = data[0:2] # data_type: HB
-        data_path = os.path.join(VIDEO_PATH, data_type, data) # MemSeg_clips/HB/HB_1.mp4
-        save_dir_path = os.path.join(SAVE_PATH, data_type, data_name) # video_frame/HB/HB_1/
-        video_data = skvideo.io.vread(data_path)[20:-20, ...] # NumPy, 20~-20: Fade-in / Fade-out
-        os.makedirs(save_dir_path, exist_ok=True) # HB/HB_1/
-
-        for i in range(0, len(video_data), 2):
-            frame_name = data_name + f"_{i}.png" # HB_1_i.png
-            im = Image.fromarray(video_data[i, ...])
-            im.save(os.path.join(save_dir_path, frame_name))
+    frame_save_dir_path = os.path.join(SAVE_PATH, boundary_type, video_name)
+    os.makedirs(frame_save_dir_path, exist_ok=True)
+    
+    video_data = skvideo.io.vread(file_path)[20:-20, ...]
+    for i, frame in enumerate(video_data):
+        frame_name = f"{video_name}_{i}.png" # HB_1_i.png
+        im = Image.fromarray(frame)
+        im.save(os.path.join(frame_save_dir_path, frame_name))
