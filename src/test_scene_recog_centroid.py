97de778e96c7dc701ab1a67893fe96b4f2905dcc
Jongheon.Kim <jgheon.kim@gmail.com>
2023-02-02 04:58:38 +0900

    fix: (enhanced) scene recognition with centroid

--- src/test_scene_recog_centroid.py
+++ src/test_scene_recog_centroid.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         num_workers=num_workers
     )
 
-    model = MODEL_DICT[MODEL_NAME](in_channels=32, latent_dim=512)
+    model = MODEL_DICT[MODEL_NAME](in_channels=32, latent_dim=512).to(device)
     model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
     scene_recog_dataset = SceneRecogDataset(root_dir=os.path.join(DATA_PATH, "MemSeg_SceneRecogImg"), transform=transform)
 
@@ -68,7 +68,7 @@ if __name__ == "__main__":
         old_set = []
         for btype in ("HB", "NB", "SB"):
             for i in tqdm(range(30)):
-                video_name = "{btype}_{i}"
+                video_name = f"{btype}_{i + 1}"
                 images, new_idx = scene_recog_dataset.get_single_data(name=video_name)
                 new_set.append(images[new_idx])
                 old_set.append(images[1 - new_idx])
@@ -82,7 +82,51 @@ if __name__ == "__main__":
         old_mu_set = old_mu_set.detach().cpu().numpy()
         # Skip log_var cause it is not necessary for now
 
-    print(video_mu_set.shape, new_mu_set.shape, old_mu_set.shape)
+    latent_set = np.concatenate([video_mu_set, new_mu_set, old_mu_set], axis=0)
+    pca = PCA(n_components=3)
+    points = pca.fit_transform(latent_set) # (8813, 512)
+    video_points, new_points, old_points = \
+        points[:len(video_mu_set)], \
+        points[len(video_mu_set):len(video_mu_set) + len(new_mu_set)], \
+        points[-len(old_mu_set):]
 
-    latent_set = np.stack(video_mu_set, new_mu_set, old_mu_set)
-    print(latent_set.shape)
+    centroid = np.mean(video_points, axis=0)
+    fig, ax = plt.subplots(1, 1, figsize=(8, 8), dpi=150, subplot_kw={"projection": "3d"})
+    ax.scatter(video_points[:, 0], video_points[:, 1], video_points[:, 2], s=2, alpha=0.2, c="green", label="Video")
+    ax.scatter(new_points[:, 0], new_points[:, 1], new_points[:, 2], s=8, c="red", label="New")
+    ax.scatter(old_points[:, 0], old_points[:, 1], old_points[:, 2], s=8, c="blue", label="Old")
+    ax.scatter(centroid[0], centroid[1], centroid[2], s=15, c="Black", label="Centroid")
+    ax.legend()
+    plt.savefig("a.png")
+
+    ###
+
+    old_dist = np.linalg.norm(old_points - centroid, axis=1)
+    new_dist = np.linalg.norm(new_points - centroid, axis=1)
+    
+    fig, ax = plt.subplots(1, 1, figsize=(6, 4), dpi=150)
+    sns.histplot(old_dist, label="Old", kde=True, linewidth=1, edgecolor='white', binwidth=1, ax=ax)
+    sns.histplot(new_dist, label="New", kde=True, linewidth=1, edgecolor='white', binwidth=1, ax=ax)
+    ax.legend()
+    plt.savefig("b.png")
+
+    ###
+
+    import sklearn.svm as svm
+    import sklearn.metrics as mt
+    from sklearn.model_selection import cross_val_score, cross_validate
+
+    svm_clf = svm.SVC(kernel='linear')
+    X = np.concatenate([old_dist, new_dist]).reshape(-1, 1)
+    y = np.array([0] * 90 + [1] * 90)
+    scores = cross_val_score(svm_clf, X, y, cv=5)
+    xaxis = [0, 1, 2, 3, 4]
+    fig, ax = plt.subplots(1, 1, figsize=(6, 4), dpi=150)
+    plt.bar(xaxis, scores, color="green")
+    ax.set_xticks(xaxis)
+    ax.set_xticklabels([f"Fold {i + 1}" for i in range(5)])
+    for i, j in zip(xaxis, scores):
+        ax.text(i, j, f"{j:.3f}", 
+            horizontalalignment='center',
+            verticalalignment='bottom')
+    plt.savefig("c.png")
